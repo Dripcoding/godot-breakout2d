b222001f1e6e97c2cda635a3420d0092d9521df0
justin <justin@pop-os.localdomain>
2023-11-25 21:59:12 -0700

    set up player movement

--- player.gd
+++ player.gd
@@ -1,8 +1,26 @@
 extends Sprite2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed:float = 400
+var screen_size
 
 func _ready():
 	print('player ready')
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+
+	if (Input.is_action_pressed('move_right')):
+		print('move right')
+		velocity.x += 1
+	elif (Input.is_action_pressed('move_left')):
+		print('move left')
+		velocity.x -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
+	# todo: use paddle width to clamp player movement
+	position = position.clamp(Vector2.ZERO, screen_size)
