27a2bc9726a1579dd27bd54bb2a72128f56476c6
justin <justin@pop-os.localdomain>
2023-11-25 22:33:53 -0700

    clean up

--- player.gd
+++ player.gd
@@ -6,17 +6,14 @@ const OFFSET_X:float = 8
 var screen_size
 
 func _ready():
-	print('player ready')
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
 	var velocity = Vector2.ZERO
 
 	if Input.is_action_pressed('move_right'):
-		print('move right')
 		velocity.x += 1
 	elif Input.is_action_pressed('move_left'):
-		print('move left')
 		velocity.x -= 1
 
 	if velocity.length() > 0:
