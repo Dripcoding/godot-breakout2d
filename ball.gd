7e92202f76ecf01daa637d0805d02b358cec9d1f
justin <justin@pop-os.localdomain>
2023-11-29 22:01:08 -0700

    wip

--- ball.gd
+++ ball.gd
@@ -1,20 +1,22 @@
-extends Sprite2D
+extends RigidBody2D
 
-@export var speed = 400
-@export var initial_x = 100
-@export var initial_y = 100
 
 func _ready():
 	print('ball ready')
-	position.x = initial_x
-	position.y = initial_y
+	# position.x = initial_x
+	# position.y = initial_y
+	# var velocity = Vector2.ZERO
+	# velocity.x = initial_velocity_x
+	# velocity.y = initial_velocity_y
 
 func _process(delta):
 	print('ball delta')
 
-	var velocity = Vector2.ZERO
+	# var velocity = Vector2.ZERO
 
-	velocity.x -= 100
+	# # velocity.x -= 100
+	# velocity.x = 0
+	# velocity.y = -100
 
-	position += velocity * delta
+	# position += velocity * delta
 	
--- player.gd
+++ player.gd
@@ -1,8 +1,5 @@
 extends Sprite2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
 func _ready():
 	print('player ready')
