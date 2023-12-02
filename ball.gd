f51db4f2f9cd04230daf92ace916bfb7eae1497c
justin <justin@pop-os.localdomain>
2023-12-01 21:16:07 -0700

    set up initial ball position

--- ball.gd
+++ ball.gd
@@ -1,22 +1,7 @@
 extends RigidBody2D
 
 
-func _ready():
-	print('ball ready')
-	# position.x = initial_x
-	# position.y = initial_y
-	# var velocity = Vector2.ZERO
-	# velocity.x = initial_velocity_x
-	# velocity.y = initial_velocity_y
-
-func _process(delta):
+func _physics_process(delta):
 	print('ball delta')
 
-	# var velocity = Vector2.ZERO
-
-	# # velocity.x -= 100
-	# velocity.x = 0
-	# velocity.y = -100
-
-	# position += velocity * delta
 	
