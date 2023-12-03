07e2764785dc4aec1370307d3a0603bca49c8716
justin <justin@pop-os.localdomain>
2023-12-02 21:42:18 -0700

    detect ball colliding with brick

--- code/ball.gd
+++ code/ball.gd
@@ -1,7 +1,14 @@
 extends RigidBody2D
 
+@onready var ball: RigidBody2D = %Ball
 
-func _physics_process(delta):
-	print('ball delta')
+var collided_with_brick: bool = false
 
-	
+func _integrate_forces(state):
+	if collided_with_brick:
+		print('collided with brick')
+
+func _on_body_entered(body:Node):
+	if body.is_in_group('brick'):
+		body.queue_free()
+		collided_with_brick = true
