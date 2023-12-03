ce41970e91139e83b12c476a0e62fff7028a7fa7
justin <justin@pop-os.localdomain>
2023-12-02 21:55:49 -0700

    impl ball-brick collision

--- code/ball.gd
+++ code/ball.gd
@@ -1,12 +1,15 @@
 extends RigidBody2D
 
+@export var ball_rotation_degrees: float = 60
+
 @onready var ball: RigidBody2D = %Ball
 
 var collided_with_brick: bool = false
 
 func _integrate_forces(state):
 	if collided_with_brick:
-		print('collided with brick')
+		collided_with_brick = false
+		state.apply_impulse(state.linear_velocity.rotated(deg_to_rad(60)))
 
 func _on_body_entered(body:Node):
 	if body.is_in_group('brick'):
