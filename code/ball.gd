a77c805f3376787eb88c743a211332503e71e09c
justin <justin@pop-os.localdomain>
2023-12-03 15:05:59 -0700

    impl ball-player collision

--- code/ball.gd
+++ code/ball.gd
@@ -1,19 +1,19 @@
 extends RigidBody2D
 
-@export var ball_rotation_degrees: float = 60
+@export var brick_collision_rotation: float = 60
+@export var player_collision_rotation: float = 60
 
 @onready var ball: RigidBody2D = %Ball
 
 var collided_with_brick: bool = false
 
-func _integrate_forces(state):
+func _integrate_forces(state: PhysicsDirectBodyState2D):
 	if collided_with_brick:
 		collided_with_brick = false
-		state.apply_impulse(state.linear_velocity.rotated(deg_to_rad(60)))
+		var rotation_radian = deg_to_rad(brick_collision_rotation)
+		state.apply_impulse(state.linear_velocity.rotated(rotation_radian))
 
-func _on_body_entered(body:Node):
+func _on_body_entered(body: Node):
 	if body.is_in_group('brick'):
 		body.queue_free()
 		collided_with_brick = true
-	elif body.is_in_group('player'):
-		print('collided with player')
