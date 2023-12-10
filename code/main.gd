0bcd507737a23bf39039795f1530586794f93d77
justin <justin@pop-os.localdomain>
2023-12-10 12:08:42 -0700

    emit out of bounds signal

---
+++ code/main.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_out_of_bounds():
+	print('main sees ball out of bounds')
--- scenes/out_of_bounds_area.gd
+++ scenes/out_of_bounds_area.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+signal out_of_bounds
+
 
 func _on_body_entered(body:Node2D):
 	if body.is_in_group('ball'):
 		body.queue_free()
+		out_of_bounds.emit()
