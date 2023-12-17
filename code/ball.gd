4584a0df8239e22cd31e52962903fedfb48ce8e2
Dripcoding <dripcoding@gmail.com>
2023-12-17 15:07:50 -0700

    set up CollidedWithWallState

--- code/ball.gd
+++ code/ball.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+class_name Ball extends RigidBody2D
 
 
 signal score
@@ -12,6 +12,10 @@ signal score
 @export var position_y_initial: float = 500
 
 
+var collided_with_brick_state = preload("res://code/ball_states/collided_with_brick_state.gd")
+var collided_with_wall_state = preload("res://code/ball_states/collided_with_wall_state.gd")
+
+
 var collided_with_brick: bool = false
 var collided_with_wall: bool = false
 var is_out_of_bounds: bool = false
@@ -22,32 +26,38 @@ var game_quit: bool = false
 var previous_velocity: Vector2 = Vector2.ZERO
 
 
+var ball_state
+var brick_state = collided_with_brick_state.new(brick_collision_rotation)
+var wall_state = collided_with_wall_state.new(wall_collision_rotation)
+
+
 func _integrate_forces(state: PhysicsDirectBodyState2D):
-	if collided_with_brick:
-		collided_with_brick = false
-		var rotation_radian = deg_to_rad(brick_collision_rotation)
-		state.apply_impulse(state.linear_velocity.rotated(rotation_radian))
-	elif collided_with_wall:
-		collided_with_wall = false
-		var rotation_radian = deg_to_rad(wall_collision_rotation)
-		state.apply_impulse(state.linear_velocity.rotated(rotation_radian))
-	elif is_out_of_bounds:
-		is_out_of_bounds = false
-		position.x = position_x_initial
-		position.y = position_y_initial
-		linear_velocity.x = Vector2.ZERO.x
-		linear_velocity.y = normal_velocity
-	elif game_over || game_quit:
-		reset()
-	elif game_resumed:
-		game_paused = false
-		game_resumed = false
-		linear_velocity = previous_velocity	
-	elif game_paused and linear_velocity != Vector2.ZERO:
-		previous_velocity = linear_velocity
-		linear_velocity = Vector2.ZERO
-	else:
-		state.linear_velocity = state.linear_velocity.normalized() * normal_velocity
+	# ball_state.handle_physics(state)
+
+	if ball_state != null:
+		if collided_with_brick:
+			collided_with_brick = false
+			ball_state.handle_physics(state)
+		elif collided_with_wall:
+			collided_with_wall = false
+			ball_state.handle_physics(state)
+		elif is_out_of_bounds:
+			is_out_of_bounds = false
+			position.x = position_x_initial
+			position.y = position_y_initial
+			linear_velocity.x = Vector2.ZERO.x
+			linear_velocity.y = normal_velocity
+		elif game_over || game_quit:
+			reset()
+		elif game_resumed:
+			game_paused = false
+			game_resumed = false
+			linear_velocity = previous_velocity	
+		elif game_paused and linear_velocity != Vector2.ZERO:
+			previous_velocity = linear_velocity
+			linear_velocity = Vector2.ZERO
+		else:
+			state.linear_velocity = state.linear_velocity.normalized() * normal_velocity
 
 
 func _on_body_entered(body: Node):
@@ -55,9 +65,10 @@ func _on_body_entered(body: Node):
 		body.hide()
 		collided_with_brick = true
 		score.emit()
+		set_state(brick_state)
 	elif body.is_in_group('wall'):
 		collided_with_wall = true
-
+		set_state(wall_state)
 
 func _on_ball_out_of_bounds(body: Node2D):
 	is_out_of_bounds = true
@@ -87,3 +98,7 @@ func reset():
 	position.x = position_x_initial
 	position.y = position_y_initial
 	linear_velocity = Vector2.ZERO
+
+
+func set_state(newState) -> void:
+	ball_state = newState
---
+++ code/ball_states/game_over_state.gd
@@ -0,0 +1,12 @@
+class_name  GameOverState
+
+
+var context = null
+
+
+func test():
+	print('game over state')
+
+
+func go_to_ready_state():
+	super.get_property_list()
