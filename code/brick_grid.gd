998b45aad61e9fa9e70a6694d0a465e0be4b545f
Dripcoding <dripcoding@gmail.com>
2023-12-18 15:45:31 -0700

    fix issue where bricks weren't reset upon quitting

--- code/brick_grid.gd
+++ code/brick_grid.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
 
-func on_game_over():
+func reset_bricks():
 	for child in get_children():
-		child.show()	
+		child.show()
+
+
+func on_game_over():
+	reset_bricks()	
+
+
+func quit():
+	reset_bricks()
